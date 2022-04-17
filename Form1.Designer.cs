fa6e25ce2d5f8b233fcdd9c9d0be2328e8d9abaf
Valon M Hoti <valon.hoti@gmail.com>
2022-04-17 16:00:28 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace WindowsFormsAppODF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(640, 354);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(640, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(659, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Load File ODS";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(659, 372);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "From File To Grid";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(658, 400);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(151, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Export GRID to ODS";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 401);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(640, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(821, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using WindowsFormsAppODF.Properties;
+
+namespace WindowsFormsAppODF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+         private void Form1_Load(object sender, EventArgs e)
+         {
+                
+         }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog theDialog = new OpenFileDialog();
+            theDialog.Title = "Open Document Spreadsheet";
+            theDialog.Filter = "Open Document Spreadsheet (.ods)|*.ods";
+          
+
+            if (theDialog.ShowDialog() == DialogResult.OK)
+            {
+                this.textBox1.Text = theDialog.FileName.ToString();
+
+                button2_Click(null, null);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataSet ds = new DataSet();
+            ds = new OdsReaderWriter().ReadOdsFile(this.textBox1.Text);
+
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private DataTable ToDataTable(DataGridView dataGridView)
+        {
+            var dt = new DataTable();
+            foreach (DataGridViewColumn dataGridViewColumn in dataGridView.Columns)
+            {
+                if (dataGridViewColumn.Visible)
+                {
+                    dt.Columns.Add();
+                }
+            }
+            var cell = new object[dataGridView.Columns.Count];
+            foreach (DataGridViewRow dataGridViewRow in dataGridView.Rows)
+            {
+                for (int i = 0; i < dataGridViewRow.Cells.Count; i++)
+                {
+                    cell[i] = dataGridViewRow.Cells[i].Value;
+                }
+                dt.Rows.Add(cell);
+            }
+            return dt;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DataSet ds = new DataSet();
+
+            DataTable dt = ToDataTable(this.dataGridView1);
+            dt.TableName = "Sheet1";
+
+            ds.Tables.Add(dt);
+
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Title = "Save Open Document Spreadsheet Files";
+            saveFileDialog1.Filter = "Open Document Spreadsheet (.ods)|*.ods";
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = saveFileDialog1.FileName;
+            }
+
+            if (this.textBox2.Text.Length>3)
+            {
+                new OdsReaderWriter().WriteOdsFile(ds, this.textBox2.Text);
+
+            }
+        }
+    }
+}
